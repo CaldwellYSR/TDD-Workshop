25a1d5425cb7c53e0e29c85414d600ce54830134
Matthew Caldwell <mcaldwell@derbywars.com>
2018-03-28 10:37:59 -0400

    Add a character with name and alignment

---
+++ spec/character.spec.js
@@ -0,0 +1,68 @@
+var Character = require('../src/character.js').character;
+var alignmentTypes = require('../src/character.js').alignmentTypes;
+
+describe("Character", function() {
+
+  beforeEach(function() {
+    this.character = new Character("George");
+  });
+
+  describe("Name", function() {
+
+    describe("getName()", function() {
+      it('should return the name', function() {
+        expect(this.character.getName()).toBe("George");
+      });
+    });
+
+    describe("setName()", function() {
+      it('should set the name', function(){
+        this.character.setName("Fred");
+        expect(this.character.getName()).toBe("Fred");
+      });
+    });
+
+  });
+
+  describe("Alignment", function() {
+
+    describe("getAlignment()", function() {
+      it('should return the alignment', function() {
+        expect(this.character.getAlignment()).toBe("Good");
+      });
+    });
+
+    describe("setAlignemnt()", function(){
+      it('should set the alignment to Evil', function(){
+        this.character.setAlignment(alignmentTypes.Evil);
+        expect(this.character.getAlignment()).toBe("Evil");
+      });
+    });
+
+    describe("validateAlignment", function() {
+      it('should return true if alignment Good',function(){
+        expect(this.character.validateAlignment(alignmentTypes.Good)).toBe(true);
+      });
+      it('should return true if alignment Neutral',function(){
+        expect(this.character.validateAlignment(alignmentTypes.Neutral)).toBe(true);
+      });
+      it('should return true if alignment Evil',function(){
+        expect(this.character.validateAlignment(alignmentTypes.Evil)).toBe(true);
+      });
+      it('should return false if given alignment is not a number', function() {
+        expect(this.character.validateAlignment("NotAnAlignment")).toBe(false);
+      });
+      it('should return false if less than 0', function(){
+        expect (this.character.validateAlignment(-1)).toBe(false);
+      });
+      it('should return false if greater than 2', function() {
+        expect (this.character.validateAlignment(3)).toBe(false);
+      });
+      it('should return false if alignment is not an integer',function(){
+        expect(this.character.validateAlignment(1.1)).toBe(false);
+      });
+    });
+
+  });
+
+});
---
+++ src/character.js
@@ -0,0 +1,43 @@
+var alignments =["Good", "Neutral", "Evil"];
+
+var alignmentTypes = {
+  Good: 0,
+  Neutral: 1,
+  Evil: 2
+}
+
+function Character(name, alignment) {
+  if(!this.validateAlignment(alignment)) {
+    alignment = alignmentTypes.Good;
+  }
+  this.name = name;
+  this.alignment = alignments[alignment];
+}
+
+Character.prototype.getName = function() {
+  return this.name;
+}
+
+Character.prototype.setName = function(name) {
+  this.name = name;
+}
+
+Character.prototype.validateAlignment = function(alignment) {
+  return typeof alignment == 'number' && Number.isInteger(alignment) && alignment >= alignmentTypes.Good && alignment <= alignmentTypes.Evil;
+}
+
+Character.prototype.getAlignment = function() {
+  return this.alignment;
+}
+
+Character.prototype.setAlignment = function(alignment){
+  if(!this.validateAlignment(alignment)) {
+    throw "That is not a valid alignment you goon! Valid alignments are [Good, Neutral, Evil] - Use alignmentTypes object as a helper"
+  }
+  this.alignment = alignments[alignment];
+}
+
+module.exports = {
+  character: Character,
+  alignmentTypes: alignmentTypes
+};
