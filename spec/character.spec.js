e4e0d72f0e20619a4e1a99e55b8f5ceda26c39a7
Matthew Caldwell <mcaldwell@derbywars.com>
2018-03-29 16:53:03 -0400

    Add test for minimum damage case

--- spec/character.spec.js
+++ spec/character.spec.js
@@ -147,7 +147,7 @@ describe("Character", function() {
     it('should add strength modifier to damage dealth', function() {
       var opponent = new Character("Fred");
 
-      // Should do 2 damage (strenght modifier is 1)
+      // Should do 2 damage (strength modifier is 1)
       this.character.strength = 12;
       this.character.attack(opponent, 15);
       expect(opponent.hitPoints).toBe(3);
@@ -155,10 +155,18 @@ describe("Character", function() {
     it('should add double strength modifier to critical damage dealth', function() {
       var opponent = new Character("Fred");
 
-      // Should do 2 damage (strenght modifier is 1)
+      // Should do 2 damage (strength modifier is 1)
       this.character.strength = 12;
       this.character.attack(opponent, 20);
       expect(opponent.hitPoints).toBe(1);
+    });
+    it('should have a minimum damage of 1 even if modifier is negative', function() {
+      var opponent = new Character("Fred");
+
+      // Should do 1 damage (strength modifier is -5)
+      this.character.strength = 1;
+      this.character.attack(opponent, 15);
+      expect(opponent.hitPoints).toBe(4);
     });
   });
 
